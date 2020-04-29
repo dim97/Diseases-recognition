871c4da4fa3b34a2ddbd4324a0361513037ca54a
dim97 <42056788+dim97@users.noreply.github.com>
2020-04-29 23:58:44 +0300

    Fixed data processing flow and added working keras model

--- NeuralNetwork/main.py
+++ NeuralNetwork/main.py
@@ -12,40 +12,75 @@ import numpy as np
 import pandas as pd
 from keras.utils import plot_model
 from keras.datasets import mnist
+from itertools import groupby
+import collections
+from keras import utils
+from sklearn.preprocessing import MultiLabelBinarizer
+from keras.callbacks.callbacks import CSVLogger
 
-#Program
+# Program
 print("\n\n\n********************** Start of main program **********************")
 datasetName = "disease_symptoms_DataSet.csv"
-print("\nLoading data", end = '')
-Dataset = pd.read_csv(datasetName , sep = ',') 
-print(colored( "  - OK",'green'))
-print("\nData examples (Head): ")
-print ("        " + Dataset.head())
-print("\nGetting all symptoms and diseases from dataset", end = '')
-AllSymptoms = Dataset.values[:,1]
+print("\nLoading data", end='')
+RawDataset = pd.read_csv(datasetName, sep=',')
+print(colored("  - OK", 'green'))
+#print("\nData examples (Head): ")
+#print ("        " + RawDataset.head())
+print("\nGetting all symptoms and diseases from dataset", end='')
+AllSymptoms = RawDataset.values[:, 1]
 AllSymptoms = list(dict.fromkeys(AllSymptoms))
 SymptomsCount = len(AllSymptoms)
-AllDiseases = Dataset.values[:,0]
+AllDiseases = RawDataset.values[:, 0]
 AllDiseases = list(dict.fromkeys(AllDiseases))
 DiseasesCount = len(AllDiseases)
-print(colored( "  - OK",'green'))
-print("Symptoms : "+ str(SymptomsCount) + "   Diseases : "+ str(DiseasesCount))
+print(colored("  - OK", 'green'))
+#print("Symptoms : "+ str(SymptomsCount) + "   Diseases : "+ str(DiseasesCount))
 
-(X,Y) = (Dataset.values[:,1],Dataset.values[:,0])
-X_train = X
+# Group symptoms by diseases
+DictDataset = collections.defaultdict(list)
+for item in RawDataset.values:
+    DictDataset[item[0]].append(item[1])
+GroupedDataset = np.array(list(DictDataset.items()))
+
+# Splitting data
+(X, Y) = (GroupedDataset[:, 1], GroupedDataset[:, 0])  # X-Symptoms, Y-Diseases
+#X_train = pd.get_dummies(pd.Series(X).apply(pd.Series).stack()).sum(level=0)
+mlb = MultiLabelBinarizer()
+X_train = pd.DataFrame(mlb.fit_transform(
+    X), columns=mlb.classes_, index=pd.DataFrame(X).index)
 X_test = X_train
+# Diseases from UMLS to array of indexes [0,0,0,....,0,1,0,...]
+Y_train = pd.get_dummies(Y)
+Y_test = Y_train
+
+pd.DataFrame(X_train).to_csv("X.csv")
+pd.DataFrame(Y_train).to_csv("Y.csv")
 
 model = keras.Sequential([
-    keras.layers.Dense(SymptomsCount, input_shape = (SymptomsCount,)),
-    keras.layers.Activation('relu'),
-    keras.layers.Dense(128,),
-    keras.layers.Activation('relu'),
-    keras.layers.Dense(DiseasesCount),
-    keras.layers.Activation('softmax'),
+    keras.layers.Dense(800, input_dim=SymptomsCount, activation="relu"),
+    keras.layers.Dense(500, input_dim=SymptomsCount, activation="relu"),
+    keras.layers.Dense(DiseasesCount, activation="softmax"),
 ])
 
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+model.compile(loss="categorical_crossentropy",
+              optimizer="SGD",
+              metrics=["accuracy"])
 
-model.summary()
+print(model.summary())
+
+csv_logger = CSVLogger('log.csv', append=True, separator=';')
+print("\nLearning start", end='')
+model.fit(
+    X_train,
+    Y_train,
+    batch_size=DiseasesCount,
+    epochs=8000,
+    verbose=1,
+    callbacks=[csv_logger]
+)
+
+print(colored("  - OK", 'green'))
+print("\nPredictions:", end='')
+predictions = model.predict(X_train)
+pd.DataFrame(predictions).to_csv("predictions.csv")
+print(predictions)
