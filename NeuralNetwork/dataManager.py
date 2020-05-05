7ce66a24147f20a0df964a6586b2e1b011e8ef76
dim97 <42056788+dim97@users.noreply.github.com>
2020-05-05 23:00:42 +0300

    code separated to multiple files

---
+++ NeuralNetwork/dataManager.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import collections
+import numpy as np
+from sklearn.preprocessing import MultiLabelBinarizer
+
+
+def LoadData(path):
+    return pd.read_csv(path, sep=',')
+
+def CountSymptoms(dataset):
+    AllSymptoms = dataset.values[:, 1]
+    AllSymptoms = list(dict.fromkeys(AllSymptoms))
+    SymptomsCount = len(AllSymptoms)
+    return SymptomsCount
+
+def CountDiseases(dataset):
+    AllDiseases = dataset.values[:, 0]
+    AllDiseases = list(dict.fromkeys(AllDiseases))
+    DiseasesCount = len(AllDiseases)
+    return DiseasesCount
+
+def GroupByDiseases(dataset):
+    DictDataset = collections.defaultdict(list)
+    for item in dataset.values:
+        DictDataset[item[0]].append(item[1])
+    GroupedDataset = np.array(list(DictDataset.items())) 
+    return GroupedDataset
+
+def SplitData(dataset):
+    return (dataset[:, 1], dataset[:, 0])
+
+def SymptomsToCategorical(groupedSymptoms):
+    # Symptoms from UMLS to array of indexes [0,1,0,....,1,1,0,...]
+    mlb = MultiLabelBinarizer()
+    X_train = pd.DataFrame(mlb.fit_transform(groupedSymptoms), columns=mlb.classes_, index=pd.DataFrame(groupedSymptoms).index)
+    return X_train
+
+def DiseasesToCategorical(groupedDiseases):
+    # Diseases from UMLS to array of indexes [0,0,0,....,0,1,0,...]
+    Y_train = pd.get_dummies(groupedDiseases)
+    return Y_train
--- NeuralNetwork/main.py
+++ NeuralNetwork/main.py
@@ -1,4 +1,4 @@
-# Use Python 3.6.* !
+# Use Python 3.6.* 
 # TensorFlow and tf.keras
 import tensorflow as tf
 #from tensorflow import keras
@@ -22,47 +22,39 @@ from keras import utils
 from sklearn.preprocessing import MultiLabelBinarizer
 from keras.callbacks import CSVLogger, EarlyStopping
 
+# Custom libraries
+import dataManager, callbackExtension
+from math import ceil
+
 # Program
 print("\n\n\n********************** Start of main program **********************")
-datasetName = "disease_symptoms_DataSet.csv"
-print("\nLoading data", end='')
-RawDataset = pd.read_csv(datasetName, sep=',')
-print(colored("  - OK", 'green'))
-#print("\nData examples (Head): ")
-#print ("        " + RawDataset.head())
-print("\nGetting all symptoms and diseases from dataset", end='')
-AllSymptoms = RawDataset.values[:, 1]
-AllSymptoms = list(dict.fromkeys(AllSymptoms))
-SymptomsCount = len(AllSymptoms)
-AllDiseases = RawDataset.values[:, 0]
-AllDiseases = list(dict.fromkeys(AllDiseases))
-DiseasesCount = len(AllDiseases)
-print(colored("  - OK", 'green'))
-#print("Symptoms : "+ str(SymptomsCount) + "   Diseases : "+ str(DiseasesCount))
+dataSetName = "disease_symptoms_DataSet.csv"
+RawDataset = dataManager.LoadData(dataSetName)
+SymptomsCount = dataManager.CountSymptoms(RawDataset)
+DiseasesCount = dataManager.CountDiseases(RawDataset)
 
 # Group symptoms by diseases
-DictDataset = collections.defaultdict(list)
-for item in RawDataset.values:
-    DictDataset[item[0]].append(item[1])
-GroupedDataset = np.array(list(DictDataset.items()))
+GroupedDataset = dataManager.GroupByDiseases(RawDataset)
 
 # Splitting data
-(X, Y) = (GroupedDataset[:, 1], GroupedDataset[:, 0])  # X-Symptoms, Y-Diseases
-#X_train = pd.get_dummies(pd.Series(X).apply(pd.Series).stack()).sum(level=0)
-mlb = MultiLabelBinarizer()
-X_train = pd.DataFrame(mlb.fit_transform(
-    X), columns=mlb.classes_, index=pd.DataFrame(X).index)
+(X, Y) = dataManager.SplitData(GroupedDataset)  # X-Symptoms, Y-Diseases
+X_train = dataManager.SymptomsToCategorical(X)
+Y_train = dataManager.DiseasesToCategorical(Y)
 X_test = X_train
-# Diseases from UMLS to array of indexes [0,0,0,....,0,1,0,...]
-Y_train = pd.get_dummies(Y)
 Y_test = Y_train
 
-pd.DataFrame(X_train).to_csv("X.csv")
-pd.DataFrame(Y_train).to_csv("Y.csv")
+pd.DataFrame(X_train).to_csv("X.csv") # save to .csv
+pd.DataFrame(Y_train).to_csv("Y.csv") # save to .csv
+
+# Defining model
+deltaOfInOut = abs(SymptomsCount-DiseasesCount)
+avgOfInOut = ceil((abs(SymptomsCount+DiseasesCount)/2))
+
+(firstLayerNeuronCount, secondLayerNeuronCount) = (ceil(avgOfInOut+deltaOfInOut/4),ceil(avgOfInOut-deltaOfInOut/4))
 
 model = keras.Sequential([
-    keras.layers.Dense(800, input_dim=SymptomsCount, activation="relu"),
-    keras.layers.Dense(500, activation="relu"),
+    keras.layers.Dense(firstLayerNeuronCount, input_dim=SymptomsCount, activation="relu"),
+    keras.layers.Dense(secondLayerNeuronCount, activation="relu"),
     keras.layers.Dense(DiseasesCount, activation="softmax"),
 ])
 
@@ -73,6 +65,7 @@ model.compile(loss="categorical_crossentropy",
 print(model.summary())
 
 csv_logger = CSVLogger('log.csv', append=True, separator=';')
+stop_criteria = callbackExtension.TerminateOnBaseline(monitor='acc', baseline=0.1)
 print("\nLearning start", end='')
 model.fit(
     X_train,
@@ -80,7 +73,7 @@ model.fit(
     batch_size=DiseasesCount,
     epochs=8000,
     verbose=1,
-    callbacks=[csv_logger]
+    callbacks=[csv_logger, stop_criteria]
 )
 
 print(colored("  - OK", 'green'))
@@ -91,4 +84,6 @@ print(predictions)
 
 #Saving model
 model.save('my_model.h5')
+#Loading model
 new_model = keras.models.load_model('my_model.h5')
+
