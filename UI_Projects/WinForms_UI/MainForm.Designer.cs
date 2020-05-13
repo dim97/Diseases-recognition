92d8fef9dce9dd09b0a77930f77366e6511032ff
dim97 <42056788+dim97@users.noreply.github.com>
2020-05-13 05:19:00 +0300

    Add C# Windows form app with example of functionality

---
+++ UI_Projects/WinForms_UI/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WinForms_UI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.PredictBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(399, 29);
+            this.OutputTextBox.Multiline = true;
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.ReadOnly = true;
+            this.OutputTextBox.Size = new System.Drawing.Size(148, 72);
+            this.OutputTextBox.TabIndex = 0;
+            this.OutputTextBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(399, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Most likely diseases:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter symptoms:";
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(12, 29);
+            this.InputTextBox.Multiline = true;
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(156, 305);
+            this.InputTextBox.TabIndex = 3;
+            // 
+            // PredictBtn
+            // 
+            this.PredictBtn.Location = new System.Drawing.Point(247, 52);
+            this.PredictBtn.Name = "PredictBtn";
+            this.PredictBtn.Size = new System.Drawing.Size(75, 23);
+            this.PredictBtn.TabIndex = 4;
+            this.PredictBtn.Text = "Predict!";
+            this.PredictBtn.UseVisualStyleBackColor = true;
+            this.PredictBtn.Click += new System.EventHandler(this.PredictBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 346);
+            this.Controls.Add(this.PredictBtn);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutputTextBox);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Disease recognition app";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox OutputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Button PredictBtn;
+    }
+}
+
