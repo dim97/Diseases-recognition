10af36bc9d63c33093c6619b2a4d1d44123f4df7
dim97 <42056788+dim97@users.noreply.github.com>
2020-05-13 05:14:23 +0300

    Add C# Windows Forms project with example of functionality

---
+++ UI_Projects/WinForms_UI/DataManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForms_UI
+{
+    public class DataManager
+    {
+        public List<Dictionary<string, double>> SortDiseasesByConfidence(List<Dictionary<string, double>> input)
+        {
+            List<Dictionary<string, double>> result = new List<Dictionary<string, double>>();
+
+            
+
+            return result;
+        }
+    }
+}
---
+++ UI_Projects/WinForms_UI/MainForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HttpRequestManager;
+using System.Windows.Forms;
+
+namespace WinForms_UI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PredictBtn_Click(object sender, EventArgs e)
+        {
+            string input = InputTextBox.Text;
+
+            input = input.Replace("\n",String.Empty);
+            input = input.Replace("\r", String.Empty);
+            input = input.Replace("\"", String.Empty);
+            input = input.Replace(" ", String.Empty);
+            List<string> symptoms = input.Split(',').ToList();
+
+            RequestManager manager = new RequestManager();
+            List<(string, double)> result = manager.GetResponse(symptoms);
+
+            var resultToShow = result.OrderByDescending(r => r.Item2).ToList();
+
+            string strToShow = "";
+            foreach (var disease in resultToShow.Take(5))
+            {
+                strToShow += disease.Item1 + Environment.NewLine;
+            }
+
+            OutputTextBox.Text = strToShow;
+        }
+    }
+}
